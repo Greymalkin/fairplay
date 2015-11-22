98ab3abfcfb86b9c58314f0be1c769e6e1c795c8
Patricia Lee <plee@automatastudios.com>
2015-11-21 23:17:49 -0500

    merging a migration

---
+++ fairplay/competition/migrations/0014_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('competition', '0013_auto_20151119_0416'),
+        ('competition', '0012_auto_20151122_0411'),
+    ]
+
+    operations = [
+    ]
