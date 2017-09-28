7a7802b8e4725b8fe18761ad6c947b38a6790b96
Patricia Lee <plee@automatastudios.com>
2017-09-28 12:16:09 -0400

    update to fixtures directory

--- fairplay/competition/management/commands/initial_setup_mag.py
+++ fairplay/competition/management/commands/initial_setup_mag.py
@@ -17,46 +17,46 @@ class Command(BaseCommand):
         check = competition.models.Event.objects.all().count()
         if not check:
             print('... loading events')
-            call_command('loaddata', 'fixtures/events.json')
+            call_command('loaddata', 'fixtures/meet_setup_mag/events.json')
 
         # shirt sizes
         check = registration.models.ShirtSize.objects.all().count()
         if not check:
             print('... loading shirt sizes')
-            call_command('loaddata', 'fixtures/shirtsizes.json')
+            call_command('loaddata', 'fixtures/meet_setup_mag/shirtsizes.json')
 
         # led shows
         check = competition.models.LEDShow.objects.all().count()
         if not check:
             print('... loading led shows')
-            call_command('loaddata', 'fixtures/ledshows.json')
+            call_command('loaddata', 'fixtures/meet_setup_mag/ledshows.json')
 
         # led signs
         check = competition.models.LEDSign.objects.all().count()
         if not check:
             print('... loading led signs')
-            call_command('loaddata', 'fixtures/ledsigns.json')
+            call_command('loaddata', 'fixtures/meet_setup_mag/ledsigns.json')
 
         # disciplines
         check = registration.models.Discipline.objects.all().count()
         if not check:
             print('... loading disciplines')
-            call_command('loaddata', 'fixtures/disciplines.json')
+            call_command('loaddata', 'fixtures/meet_setup_mag/disciplines.json')
 
         # if there is no meet in the database at all, set up an example one
         check = meet.models.Meet.objects.all().count()
         if not check:
             print('... loading example meet')
-            call_command('loaddata', 'fixtures/meet.json')
+            call_command('loaddata', 'fixtures/meet_setup_mag/meet.json')
 
             print('... loading levels')
-            call_command('loaddata', 'fixtures/levels.json')
+            call_command('loaddata', 'fixtures/meet_setup_mag/levels.json')
 
             print('... loading divisions')
-            call_command('loaddata', 'fixtures/divisions.json')
+            call_command('loaddata', 'fixtures/meet_setup_mag/divisions.json')
 
             print('... loading team awards')
-            call_command('loaddata', 'fixtures/awards.json')
+            call_command('loaddata', 'fixtures/meet_setup_mag/awards.json')
 
             print('... associating levels to divisions and team awards')
             print('    Level 4')
