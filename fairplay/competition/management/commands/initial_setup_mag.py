9dcab41da767a84e1937d5715d4d4eda6a06c338
Patricia Lee <plee@automatastudios.com>
2017-10-11 14:39:23 -0400

    updating age divisions in the Fairplay MAG starter json

--- fairplay/competition/management/commands/initial_setup_mag.py
+++ fairplay/competition/management/commands/initial_setup_mag.py
@@ -1,62 +1,36 @@
-import meet
-import registration
-import competition
-
 from django.core.management import call_command
 from django.core.management.base import BaseCommand
 
 
 class Command(BaseCommand):
-    """ Initial Setup can be called from a link that appears on the dashboard
-        only when there are 0 Meets in the db """
+    """ Initial Setup can be called from a link that appears on the dashboard """
 
     def handle(self, *args, **kwargs):
-        print('Evaluating status of Fairplay system ...')
+        print('Installing Fairplay starter, men\'s meet...')
 
-        # events
-        check = competition.models.Event.objects.all().count()
-        if not check:
-            print('... loading events')
-            call_command('loaddata', 'fixtures/meet_setup_mag/events.json')
+        print('... events')
+        call_command('loaddata', 'fixtures/meet_setup_mag/events.json')
 
-        # shirt sizes
-        check = registration.models.ShirtSize.objects.all().count()
-        if not check:
-            print('... loading shirt sizes')
-            call_command('loaddata', 'fixtures/meet_setup_mag/shirtsizes.json')
+        print('... shirt sizes')
+        call_command('loaddata', 'fixtures/meet_setup_mag/shirtsizes.json')
 
-        # led shows
-        check = competition.models.LEDShow.objects.all().count()
-        if not check:
-            print('... loading led shows')
-            call_command('loaddata', 'fixtures/meet_setup_mag/ledshows.json')
+        print('... led shows')
+        call_command('loaddata', 'fixtures/meet_setup_mag/ledshows.json')
 
-        # led signs
-        check = competition.models.LEDSign.objects.all().count()
-        if not check:
-            print('... loading led signs')
-            call_command('loaddata', 'fixtures/meet_setup_mag/ledsigns.json')
+        print('... led signs')
+        call_command('loaddata', 'fixtures/meet_setup_mag/ledsigns.json')
 
-        # disciplines
-        check = registration.models.Discipline.objects.all().count()
-        if not check:
-            print('... loading disciplines')
-            call_command('loaddata', 'fixtures/meet_setup_mag/disciplines.json')
+        print('... disciplines')
+        call_command('loaddata', 'fixtures/meet_setup_mag/disciplines.json')
 
-        # if there is no meet in the database at all, set up an example one
-        check = meet.models.Meet.objects.all().count()
-        if not check:
-            print('... loading mag meet')
-            call_command('loaddata', 'fixtures/meet_setup_mag/meet.json')
+        print('... men\'s meet')
+        call_command('loaddata', 'fixtures/meet_setup_mag/meet.json')
 
-            print('... loading levels')
-            call_command('loaddata', 'fixtures/meet_setup_mag/levels.json')
+        print('... men\'s levels')
+        call_command('loaddata', 'fixtures/meet_setup_mag/levels.json')
 
-            print('... loading divisions')
-            call_command('loaddata', 'fixtures/meet_setup_mag/divisions.json')
+        print('... men\'s divisions')
+        call_command('loaddata', 'fixtures/meet_setup_mag/divisions.json')
 
-            print('... loading team awards')
-            call_command('loaddata', 'fixtures/meet_setup_mag/awards.json')
-
-        print('')
-        print('Initial meet setup is now complete')
+        print('... men\'s team awards')
+        call_command('loaddata', 'fixtures/meet_setup_mag/awards.json')
