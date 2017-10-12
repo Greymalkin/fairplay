0de87805c75d477aa5b2e59faa09d5832dc77c69
Patricia Lee <plee@automatastudios.com>
2017-10-12 00:12:34 -0400

    need to set up logging, so putting logging variables in the settings

--- fairplay/fairplay/settings/local.py
+++ fairplay/fairplay/settings/local.py
@@ -25,3 +25,33 @@ STATIC_ROOT = 'local_static'
 
 # Django Debug Toolbar
 INTERNAL_IPS = ('127.0.0.1', 'localhost')
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'standard': {
+            'format': "%(asctime)s.%(msecs).03d %(levelname)s [%(module)s:%(lineno)s] %(message)s",
+            'datefmt': "%Y-%m-%d %H:%M:%S"
+        },
+    },
+    'handlers': {
+        'console': {
+            'level': 'INFO',
+            'class': 'logging.StreamHandler',
+            'formatter': 'standard'
+        },
+    },
+    'root': {
+        'handlers': ['console', ],
+        'propagate': True,
+        'level': 'WARNING',
+    },
+    'loggers': {
+        'conductor': {
+            'handlers': ['console', ],
+            'propagate': False,
+            'level': 'INFO',
+        }
+    },
+}
