156421a3c9291825ddb9c386baa9950ad29b3ec0
Branden Hall <bhall@automatastudios.com>
2018-07-08 22:55:31 -0400

    added deletion of old venv and old database to installer

--- install.bat
+++ install.bat
@@ -1,9 +1,10 @@
 @echo off
 echo This application will install Fairplay.
-echo
+echo.
 pause
 echo Installing Fairplay . . .
+del /Q fairplay\fairplay_db
+rmdir /S /Q venv
 python -m venv venv
-del fairplay\fairplay_db
 venv\Scripts\activate.bat & pip install -r requirements\windows.txt & cls & echo Setting up Fairplay database . . . & venv\Scripts\activate.bat & cd fairplay & python manage.py migrate --settings=fairplay.settings.windows & cls & echo Creating new admin user for Fairplay . . . & python manage.py createsuperuser --username=admin --email=admin@admin.com --settings=fairplay.settings.windows
 pause
