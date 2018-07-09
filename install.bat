ae29147a8b3fac69f32ae88da5229eb6a3b26348
Branden Hall <bhall@automatastudios.com>
2018-07-08 23:34:54 -0400

    nulled out install errors and added menu to scoreboard controls

--- install.bat
+++ install.bat
@@ -3,8 +3,8 @@ echo This application will install Fairplay.
 echo.
 pause
 echo Installing Fairplay . . .
-del /Q fairplay\fairplay_db
-rmdir /S /Q venv
+del /Q fairplay\fairplay_db >nul 2>&1
+rmdir /S /Q venv >nul 2>&1
 python -m venv venv
 venv\Scripts\activate.bat & pip install -r requirements\windows.txt & cls & echo Setting up Fairplay database . . . & venv\Scripts\activate.bat & cd fairplay & python manage.py migrate --settings=fairplay.settings.windows & cls & echo Creating new admin user for Fairplay . . . & python manage.py createsuperuser --username=admin --email=admin@admin.com --settings=fairplay.settings.windows
 pause
