aa4c208839bdc75973f8511c5a652c7ddc913c74
Branden Hall <bhall@automatastudios.com>
2018-07-08 22:41:24 -0400

    fix requirements for windows to not have psycopg2 and make installer delete db before starting

--- install.bat
+++ install.bat
@@ -1,7 +1,9 @@
 @echo off
-echo This script will install Fairplay.
+echo This application will install Fairplay.
+echo
 pause
 echo Installing Fairplay . . .
 python -m venv venv
+del fairplay\fairplay_db
 venv\Scripts\activate.bat & pip install -r requirements\windows.txt & cls & echo Setting up Fairplay database . . . & venv\Scripts\activate.bat & cd fairplay & python manage.py migrate --settings=fairplay.settings.windows & cls & echo Creating new admin user for Fairplay . . . & python manage.py createsuperuser --username=admin --email=admin@admin.com --settings=fairplay.settings.windows
 pause
